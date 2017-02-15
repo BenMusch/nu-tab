62174614a0f4c65bc857a56ac1f8b33dd902ca06
Ben Muschol <benmuschol@gmail.com>
2017-02-14 19:39:36 -0500

    hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    render text: 'Hello world'
+  end
 end
