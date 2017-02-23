bdd3eeb735d836721aac3f393360f72d3951a015
Ben Muschol <benmuschol@gmail.com>
2017-02-22 19:46:56 -0500

    Rails best practice config to ignore STI method

--- app/models/debater.rb
+++ app/models/debater.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Debater < ApplicationRecord
   belongs_to :school
 end
--- app/models/judge_school.rb
+++ app/models/judge_school.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class JudgeSchool < ApplicationRecord
   belongs_to :judge
   belongs_to :school
