2088452411147e3824b66d58ada0bddca9df1566
Ben Muschol <benmuschol@gmail.com>
2017-03-04 23:10:30 -0500

    the commit of shame

--- app/policies/stats/round.rb
+++ app/policies/stats/round.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 module Stats
   module Round
+    # rubocop:disable Metrics/PerceivedComplexity
     def self.policy_for(team, round)
       if round.bye? || round.all_win?
         AverageStatsPolicy
@@ -10,6 +11,7 @@ module Stats
         round.winner?(team) ? AverageStatsPolicy : self.forfeit_policy
       end
     end
+    # rubocop:enable Metrics/PerceivedComplexity
 
     def self.forfeit_policy
       TournamentSetting.get_bool('punish_forfeits') ? PunitivePolicy : AverageStatsPolicy
