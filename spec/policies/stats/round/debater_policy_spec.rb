45636022c68ddd9ef384274a346c9ea44074680c
Ben Muschol <benmuschol@gmail.com>
2017-03-04 20:50:49 -0500

    specs for average round policy

--- spec/policies/stats/round/debater_policy_spec.rb
+++ spec/policies/stats/round/debater_policy_spec.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe Stats::Round::StandardPolicy do
+RSpec.shared_context 'it has a full round' do
   let(:team)       { create(:team) }
   let(:other_team) { create(:team) }
   let(:debater)    { create(:debater, team: team) }
-  let(:round)      { create(:round, gov_team: team, opp_team: other_team) }
+  let(:round)      { create(:round, gov_team: team, opp_team: other_team, round_number: 2) }
+end
+
+RSpec.describe Stats::Round::StandardPolicy do
+  include_context 'it has a full round'
   let(:policy)     { described_class.new(debater, round) }
   let!(:stats) do
     create(:debater_round_stat, round: round, debater: debater, speaks: 25.5, ranks: 3)
@@ -30,10 +34,7 @@ RSpec.describe Stats::Round::StandardPolicy do
 end
 
 RSpec.describe Stats::Round::PunitivePolicy do
-  let(:team)       { create(:team) }
-  let(:other_team) { create(:team) }
-  let(:debater)    { create(:debater, team: team) }
-  let(:round)      { create(:round, gov_team: team, opp_team: other_team) }
+  include_context 'it has a full round'
   let(:policy)     { described_class.new(debater, round) }
 
   describe '#speaks' do
@@ -45,6 +46,58 @@ RSpec.describe Stats::Round::PunitivePolicy do
   describe '#ranks' do
     it "returns the ranks of the debater's stats for the round" do
       expect(policy.ranks).to eq 3.5
+    end
+  end
+end
+
+RSpec.describe Stats::Round::AverageStatsPolicy do
+  include_context 'it has a full round'
+  let(:policy)     { described_class.new(debater, round) }
+
+  before do
+    debater = create(:debater, team: create(:team))
+    round = create(:round, gov_team: debater.team, opp_team: create(:team), round_number: rand(5))
+    create(:debater_round_stat, round: round, debater: debater, speaks: 26, ranks: 3)
+
+    debater = create(:debater, team: create(:team))
+    round = create(:round, gov_team: debater.team, opp_team: create(:team), round_number: rand(5))
+    create(:debater_round_stat, round: round, debater: debater, speaks: 26.25, ranks: 4)
+  end
+
+  context 'with other round stats in the database' do
+    let(:round1) { create(:round, gov_team: team, opp_team: create(:team), round_number: 1) }
+    let(:round3) { create(:round, gov_team: team, opp_team: create(:team), round_number: 3) }
+    let!(:round1_stats) do
+      create(:debater_round_stat, round: round1, debater: debater, speaks: 25, ranks: 1)
+    end
+    let!(:round2_stats) do
+      create(:debater_round_stat, round: round3, debater: debater, speaks: 25.25, ranks: 2)
+    end
+
+    describe '#speaks' do
+      it 'averages all of the speaks for the debater' do
+        expect(policy.speaks).to eq 25.125
+      end
+    end
+
+    describe '#ranks' do
+      it 'averages all of the ranks for the debater' do
+        expect(policy.ranks).to eq 1.5
+      end
+    end
+  end
+
+  context 'without other round stats in the database' do
+    describe '#speaks' do
+      it 'averages speaks for all of the debaters' do
+        expect(policy.speaks).to eq 26.125
+      end
+    end
+
+    describe '#ranks' do
+      it 'averages ranks for all of the debaters' do
+        expect(policy.ranks).to eq 3.5
+      end
     end
   end
 end
