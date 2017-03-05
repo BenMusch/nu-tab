2cc1630d2f5d2c10d52cb2b312976427c2605d7c
Ben Muschol <benmuschol@gmail.com>
2017-03-05 14:21:00 -0500

    add index

--- db/migrate/20170305182003_add_school_to_teams.rb
+++ db/migrate/20170305182003_add_school_to_teams.rb
@@ -2,5 +2,6 @@ class AddSchoolToTeams < ActiveRecord::Migration[5.0]
   def change
     add_column :teams, :school_id, :integer
     add_foreign_key :teams, :schools
+    add_index :teams, [:school_id]
   end
 end
