c248f1f5ba2fc7460afb7e98e282095024105f27
Ben Muschol <benmuschol@gmail.com>
2017-03-26 22:43:33 +0200

    pairing generator

--- app/policies/pairing/pairing_generator.rb
+++ app/policies/pairing/pairing_generator.rb
@@ -7,12 +7,21 @@ module Pairing
       @teams = if round_number == 1
                  teams.sort
                else
-                 teams.sort_by(&:seed)
+                 # sort by seed, then randomly shuffle amongst equal seeds
+                 teams.sort do |a, b|
+                   return b.seed - a.seed unless a.seed == b.seed
+                   rand(2).zero? ? -1 : 1
+                 end
                end
     end
 
-    def generate
+    def generate!
       teams.delete!(bye.try(:team))
+      raw_pairings.each do |pairing|
+        gov, opp = assign_sides(pairing)
+
+        Round.new(round_number: round_number, gov_team: gov, opp_team: opp)
+      end
     end
 
     private
@@ -20,7 +29,29 @@ module Pairing
     attr_reader :teams, :round_number
 
     def bye
-      @bye ||= ByeGenerator.generate
+      @bye ||= ByeGenerator.new(teams).generate!
+    end
+
+    def brackets
+      @brackets ||= BracketGenerator.new(teams).generate!
+    end
+
+    def raw_pairings
+      @raw_pairings ||= brackets.map do |bracket|
+        MaximumWeightMatcher.new(bracket).match!
+      end.flatten
+    end
+
+    # expect an array of 2 teams, returns with the gov team as [gov, opp] based
+    # on previous gov/opp counts
+    def assign_sides(pairing)
+      if pairing[0].govs.count != pairing[1].govs.count
+        pairing.sort_by { |team| team.govs.count }
+      elsif pairing[0].opps.count != pairing[1].opps.count
+        pairing.sort_by { |team| team.opps.count }
+      else
+        pairing.shuffle
+      end
     end
   end
 end
