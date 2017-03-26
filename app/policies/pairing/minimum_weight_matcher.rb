343ec7cb6a3c43e6f495cb413bace1e0e7f0ffe6
Ben Muschol <benmuschol@gmail.com>
2017-03-26 14:12:38 +0200

    clean bundle

---
+++ app/policies/pairing/minimum_weight_matcher.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# uses the graph_matching gem to generate a minimum-weight matching for the
+# passed bracket
+class MinimumWeightMatcher
+  def initialize(bracket)
+    @bracket = bracket
+  end
+end
