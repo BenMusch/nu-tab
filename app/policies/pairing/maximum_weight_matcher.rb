fc43499067201ee184fe01f7cb33cb7dfafd503a
Ben Muschol <benmuschol@gmail.com>
2017-03-26 21:57:33 +0200

    codeclimatE

--- app/policies/pairing/maximum_weight_matcher.rb
+++ app/policies/pairing/maximum_weight_matcher.rb
@@ -9,6 +9,7 @@ module Pairing
       # store the integer keys for each team because the graph matching library
       # requires consecutive, positive integers for vertices
       @vertex_nums = {}
+      bracket.each { |team| vertex_nums[team] = bracket.find_index(team) + 1 }
     end
 
     # Returns the optimal matching of the teams, where each pairing is an array
@@ -27,11 +28,12 @@ module Pairing
         team1 = pairing[0]
         team2 = pairing[1]
 
-        vertex_nums[team1] ||= bracket.find_index(team1) + 1
-        vertex_nums[team2] ||= bracket.find_index(team2) + 1
-
-        [vertex_nums[team1], vertex_nums[team2], Pairing::Penalty.calculate(team1, team2, bracket)]
+        [vertex_num(team1), vertex_num(team2), Pairing::Penalty.calculate(team1, team2, bracket)]
       end
+    end
+
+    def vertex_num(team)
+      vertex_nums[team]
     end
 
     def graph
